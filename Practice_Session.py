87215c1b42108fb0809d9467479096427d8339ff
naveen.kstb <naveenkumarst11@gmail.com>
2023-06-26 16:20:26 +0530

    this is my first commit

---
+++ Practice_Session.py
@@ -0,0 +1,10 @@
+# def an object to Find odd or even number in one function
+# Then find the Prime
+
+
+#Find the given number is odd or even using Python
+num=int(input("Enter a number for check odd or even: "))
+if(num%2)==0:
+    print(num," Is even")
+elif(num%2)==1:
+    print(num," is odd")
